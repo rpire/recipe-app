cf1e4551c43720519cc6dc32f0391f9747370ce1
Reshy <reshy@protonmail.com>
2021-12-20 21:18:16 +0100

    Add index view for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,26 @@
 class RecipesController < ApplicationController
+    load_and_authorize_resource
+    
+    def index
+        @recipes = current_user.recipes
+    end
+
+    def show
+
+    end
+
+    def create
+        recipe = current_user.recipe.new(text: comment_params[:text], user: current_user)
+        respond_to do |format|
+          format.html do
+            if recipe.save
+              flash[:notice] = 'Recipe was successfully created.'
+            else
+              flash[:alert] = 'Failed to add recipe!'
+            end
+            redirect_to recipes_path
+          end
+        end
+      end
+      
 end
