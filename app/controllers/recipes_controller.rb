4cb143cc4da672c78e98f6f9babc5ae8db6dac67
Reshy <reshy@protonmail.com>
2021-12-20 23:08:24 +0100

    Add delete button for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,29 @@
 class RecipesController < ApplicationController
     load_and_authorize_resource
-    
+
     def index
-        @recipes = current_user.recipes
+        @recipes = current_user.recipes.all
     end
 
     def show
+        @recipe = Recipe.find(params[:id])
+    end
 
+    def destroy
+        recipe = Recipe.find(params[:id])
+        if recipe.destroy
+            flash[:notice] = 'Recipe deleted!'
+        else
+            flash[:alert] = 'Failed to delete recipe!'
+        end
+        redirect_to recipes_path
     end
 
     def create
-        recipe = current_user.recipe.new(text: comment_params[:text], user: current_user)
+        @recipe = current_user.recipes.new(create_params)
         respond_to do |format|
           format.html do
-            if recipe.save
+            if @recipe.save
               flash[:notice] = 'Recipe was successfully created.'
             else
               flash[:alert] = 'Failed to add recipe!'
@@ -21,6 +31,11 @@ class RecipesController < ApplicationController
             redirect_to recipes_path
           end
         end
+    end
+
+    private
+
+    def create_params
+        params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
       end
-      
 end
--- db/migrate/20211220105356_add_recipes_ref_to_recipe_food.rb
+++ db/migrate/20211220105356_add_recipes_ref_to_recipe_food.rb
@@ -1,5 +1,6 @@
 class AddRecipesRefToRecipeFood < ActiveRecord::Migration[6.1]
   def change
-    add_reference :recipe_foods, :recipes, null: false, foreign_key: true
+    add_reference :recipe_foods, :recipe, null: false, foreign_key: true
+    # ALTER TABLE recipe_foods RENAME COLUMN recipes_id TO recipe_id;
   end
 end
