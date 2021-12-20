b82896379b11a9ea5df581930440c1893f7d019e
Reshy <reshy@protonmail.com>
2021-12-20 15:59:47 +0100

    Add rubocop linter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
 # app/models/ability.rb
 class Ability
-
   # Add in CanCan's ability definition DSL
   include CanCan::Ability
 
-  def initialize( user )
-
+  def initialize(user)
     # Handle the case where we don't have a current_user i.e. the user is a guest
     return unless user.present?
 
