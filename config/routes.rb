d64109aebeb8de12fdc241642eb6010030b1397b
rpire <rubenpire7@gmail.com>
2021-12-20 09:29:00 -0400

    Create the routes for the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'public_recipes#index'
+  resources :recipes, only: [:index, :new, :show, :create]
+  resources :foods, only: [:index, :new, :create]
+  resources :public_recipes, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
