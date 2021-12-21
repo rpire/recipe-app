40cdd7a37f72029a7677c9472b56a01dc5ed6b8e
rpire <rubenpire7@gmail.com>
2021-12-21 09:09:12 -0400

    Add the view for the food list route

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,35 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = current_user.foods.all
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    if food.destroy
+      flash[:notice] = 'Food removed!'
+    else
+      flash[:alert] = 'Failed to remove food!'
+    end
+    redirect_to foods_path
+  end
+
+  def create
+    @food = current_user.foods.new(create_params)
+    respond_to do |format|
+      format.html do
+        if @food.save
+          flash[:notice] = 'Food was successfully added.'
+        else
+          flash[:alert] = @food.errors.full_messages.first
+        end
+        redirect_to foods_path
+      end
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+  validates :price, numericality: true
 end
