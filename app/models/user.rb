9f35fb9323649f099e2420675a11e66350d9cf63
rpire <rubenpire7@gmail.com>
2021-12-23 12:27:42 -0400

    Remove email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, :email, presence: true
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
